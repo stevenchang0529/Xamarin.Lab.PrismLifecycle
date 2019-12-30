a747a90bb59b511ecd81a0c9474704e937afbe49
Steven <steven@MSI-Steven>
2019-12-30 16:23:31 +0800

    add

---
+++ Xamarin.Lab.PrismLifecycle/Xamarin.Lab.PrismLifecycle/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.AppModel;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Xamarin.Lab.PrismLifecycle.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase, IApplicationLifecycleAware,IPageLifecycleAware
+    {
+        public ICommand OnNext { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            this.OnNext = new DelegateCommand(() =>
+              {
+                  this.NavigationService.NavigateAsync("PrismContentPage1");
+              });
+        }
+
+        public void OnAppearing()
+        {
+          
+        }
+
+        public void OnDisappearing()
+        {
+            
+        }
+
+        public void OnResume()
+        {
+           
+        }
+
+        public void OnSleep()
+        {
+           
+        }
+    }
+}
---
+++ Xamarin.Lab.PrismLifecycle/Xamarin.Lab.PrismLifecycle/Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.Lab.PrismLifecycle.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
